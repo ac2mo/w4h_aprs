7fb4293ef0919cf3f19dfd80155af4076f4c9b76
Gregory S. Hoerner (AC2MO) <wnypcrepair@gmail.com>
2017-06-07 00:33:01 -0400

    Initial skeleton

---
+++ inc/w4h-aprs-widget.php
@@ -0,0 +1,7 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Go Away!' );
+
+class w4h-aprs-widget extends WP_Widget {
+
+}
+?>
---
+++ w4h-aprs.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name:	W4H-APRS
+Plugin URI:	https://github.com/ac2mo/w4h-aprs
+Description:	Places an APRS tracking widget on your site.
+Version:	0.1
+Author:		Gregory S. Hoernern (AC2MO)
+License:	MIT
+*/
+defined( 'ABSPATH' ) or die( 'Go Away!' );
+
+class w4h-aprs {
+
+	public function __construct() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts_and_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_public_scripts_and_styles' ) );
+
+		register_activation_hook( __FILE__, array( $this, 'plugin_activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'plugin_deactivate' ) );
+	}
+
+	public function enqueue_admin_scripts_and_styles() {
+		wp_enqueue_style( 'w4h-aprs-admin', plugin_dir_url( __FILE__ ) . '/css/w4h-aprs-admin.css' );
+	}
+
+	public function enqueue_public_scripts_and_styles() {
+		wp_enqueue_style( 'w4h-aprs-public', plugin_dir_url( __FILE__ ) . '/css/w4h-aprs-public.css' );
+	}
+
+	public function plugin_activate() {
+	}
+
+	public function plugin_deactivate() {
+	}
+}
+
+include( plugin_dir_path( __FILE__ ) . 'inc/w4h-aprs-widget.php' );
+?>
